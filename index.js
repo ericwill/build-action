96281f6717b3ee74ad8e6379fcb32d40901bf0f1
Eric Williams <ericwill@redhat.com>
2020-04-20 14:38:05 -0400

    Testing stdout and stderr
    Signed-off-by: Eric Williams <ericwill@redhat.com>

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const { spawn } = require('child_process')
 
 try {
   const child = spawn('pwd');
-  child.on('exit', function (code, signal) {
-    console.log('child process exited with ' +
-                `code ${code} and signal ${signal}`);
+  child.stdout.on('data', (data) => {
+    console.log(`child stdout:\n${data}`);
+  });
+  child.stderr.on('data', (data) => {
+    console.error(`child stderr:\n${data}`);
   });
   // `who-to-greet` input defined in action metadata file
   const messagePassed = core.getInput('message');
